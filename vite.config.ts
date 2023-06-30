a46cdc3ec1b7c17e206e2f18c993eb662eb0c6c2
s3rbug <bugaychuk.sergiy@gmail.com>
2023-06-30 18:53:48 +0300

    vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import { config } from './src/utils/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${config.ROUTER_BASE_URL}`
 });
